cdb4ad3637ea3711aaf433ff54cf66148e17a079
M-Azariel <walter.azariel.moreno@gmail.com>
2024-12-27 13:16:03 -0300

    arreglando interlineado

--- src/components/ScreenCoutries/CantAndSearch.jsx
+++ src/components/ScreenCoutries/CantAndSearch.jsx
@@ -5,10 +5,10 @@ export const CantAndSearch = ({ setFilter, countries }) => {
 
     return (
         <section className="flex flex-col sm:flex-row justify-between items-center w-full mt-10 font-bold xl:text-xl lg:text-lg">
-            <div className="xl:ml-[94px] lg:ml-[60px] sm:ml-6 xl:text-2xl text-gray-600 mb-5 sm:mb-0">
+            <div className="xl:ml-[107px] lg:ml-[60px] sm:ml-6 xl:text-2xl text-gray-600 mb-5 sm:mb-0">
                 <p>Found {countries.length} countries</p>
             </div>
-            <div className="xl:mr-[105px] lg:mr-[55px] sm:mr-5 relative flex items-center mb-5 sm:mb-0 w-full sm:w-auto">
+            <div className="xl:mr-[83px] lg:mr-[55px] sm:mr-5 relative flex items-center mb-5 sm:mb-0 w-full sm:w-auto">
                 <img src="/Search.svg" alt="" className="absolute pl-2 w-10" />
                 <input
                     type="search"
